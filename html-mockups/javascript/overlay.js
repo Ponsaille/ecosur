8dbdccb359aedd9dcf6a1dae86acad9bf916baeb
Jules Ponsaillé <jules.ponsaille@isep.fr>
2019-03-11 10:24:12 +0100

    ajout de l'overlay de connexion

---
+++ html-mockups/javascript/overlay.js
@@ -0,0 +1,28 @@
+class Overlay {
+    constructor(overlay, button) {
+        this.overlay = overlay;
+        this.button = button;
+        this.closeButtons = this.overlay.getElementsByClassName('overlay_close');
+        this.button.addEventListener('click', this.open);
+        for(let i = 0; i < this.closeButtons.length; i++) {
+            console.log(this.closeButtons[i]);
+            this.closeButtons[i].addEventListener('click', this.close);
+        }
+    }
+
+    toggle = () => {
+        this.overlay.classList.toggle('open');
+    }
+
+    open = () => {
+        if(!this.overlay.classList.contains('open')) {
+            this.overlay.classList.add('open')
+        }
+    }
+
+    close = () => {
+        if(this.overlay.classList.contains('open')) {
+            this.overlay.classList.remove('open')
+        }
+    }
+}
