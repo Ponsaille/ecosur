08194d631565f9fe059e2df45fc802ee931e9090
Jules Ponsaillé <jules.ponsaille@isep.fr>
2019-04-14 14:52:34 +0200

    ajout du code pour l'erreur d'accès

--- MVC/app/controllers/AuthController.php
+++ MVC/app/controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 class AuthController extends Controller {
     function __construct() {
         if(!isset($_SESSION['user_id'])) {
+            http_response_code(403);
             die($this->view('errors/403'));
         }
     }
