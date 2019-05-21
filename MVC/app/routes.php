fc4a78d71d21ec88a2413b23d7f31b04dc0d3007
Laurianne <laurianne.progent@isep.fr>
2019-05-21 15:20:08 +0200

    pre-merge

--- MVC/app/routes.php
+++ MVC/app/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-    Ici on définit les routes
-
-    EXEMPLE
-    Le chemin (ce qui est marqué dans l'url après le nom du site) est "/exemple/exemplePage"
-    On veut appeler une méthode du Contrôleur (qui permettra derrière d'afficher la bonne vue), pour cela on utilise la
-        méthode get() définie dans le Routeur :
-    $router->get('/exemple/exemplePage', 'NomContrôlleur@NomMéthode');
- */
-
-
 $router->get('', 'StaticController@home');
 $router->get('faq', 'StaticController@faq');
 $router->post('inscription', 'UsersController@inscription');
