863bb1f00c726ea939f8f721d4aa8cfd1d58a934
Ponsaille <jules.ponsaille@isep.fr>
2019-06-04 22:34:10 +0200

    modification de mot de passe

--- src/app/views/users/users-edit.view.php
+++ src/app/views/users/users-edit.view.php
@@ -31,6 +31,7 @@
                     <span>Pays</span>
                     <input type="text" name="pays" value="<?= rawurldecode($user->pays) ?>">
                 </label>
+                <a style="padding: 5px" class="btn-gray" href="/recuperation/1">Modifier votre mot de passe</a> <br><br>
                 <input class="btn-gray" type="submit" value="Editer"> | <a href="/delete-user" onclick="return confirm('Êtes-vous sûr ?')" class="supprimerCapteur">Supprimer le compte</a>
             </form>
         </section>
